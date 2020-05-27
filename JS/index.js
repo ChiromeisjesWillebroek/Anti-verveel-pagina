5097928c4abbe51d7f7e9a969599798b21e0b5e7
YmkeVandebroek <51299350+YmkeVandebroek@users.noreply.github.com>
2020-05-27 15:44:01 +0200

    Melding aangepast

--- JS/index.js
+++ JS/index.js
@@ -8,9 +8,3 @@ function willekeurigGetal(min, max) {
 }
 
 let video1 = "https://www.youtube.com/watch?v=NX5xIRUcPyI";
-
-/*Functie om een waarschuwing te tonen wanneer de pagina laadt*/
-let body = document.getElementsByTagName("body")[0];
-body.onload = function(){
-  alert("!BELANGRIJK! Merk je als je verdergaat dat deze pagina niet goed werkt? Het helpt om hem dan te openen in Chrome.");
-};
